066e701a4f171e7dd703622a617e09ffd375a6ad
zerone0x <soberzml2333@gmail.com>
2024-01-22 19:10:02 +0800

    🔧 chore(index.html): add CSS styling and fix typo in play button id 🔧 chore(script.js): refactor playRound function and add event listeners to buttons 🔧 chore(style.css): add CSS styling for body and choices
    The changes in index.html include adding a link to the style.css file, adding a title div, fixing a typo in the play button id, and adding buttons for rock, paper, and scissors. The script.js file has been updated to refactor the playRound function, add event listeners to the buttons, and update the result and score divs. Lastly, the style.css file has been created and includes CSS styling for the body and choices. These changes improve the appearance and functionality of the Rock Paper Scissors game.

--- script.js
+++ script.js
@@ -3,43 +3,67 @@ function getComputerChoice(){
     const randomNumber =   Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
+const rock_btn = document.querySelector('#rock');
+const paper_btn = document.querySelector('#paper');
+const scissors_btn = document.querySelector('#scissors');
+rock_btn.onclick = function(){
+    // console.log("rock");
+    playRound('rock');
+}
+paper_btn.onclick = function(){
+    playRound('paper');
+}
+scissors_btn.onclick = function(){
+    playRound('scissors');
+}
+const result = document.querySelector('#result');
+const score = document.querySelector('#score')
+score.textContent = `you win 0 times, you lose 0 times, draw 0 times`;
+let res = [0,0,0];
 
-function playRound(playerSelection, computerSelection){
-    playerSelection=prompt("pls input:");
+function play(){
+    res = [0,0,0];
+    const result = document.querySelector('#result');
+    const score = document.querySelector('#score')
+    result.textContent = 'game start';  
+    score.textContent = `you win 0 times, you lose 0 times, draw 0 times`;
+}
+
+function playRound(playerSelection){
+    // playerSelection=prompt("pls input:");
+    if(res[0]==5 || res[1]==5){
+        result.textContent = 'game over';  
+        if (res[0]>res[1]){
+            result.textContent += 'you win';
+        }else{
+            result.textContent += 'you lose';
+        } 
+        return;
+    }
     computerSelection=getComputerChoice();
     playerSelection=playerSelection.toLowerCase();
     if ((playerSelection== 'rock' && computerSelection=='paper' ) || (playerSelection== 'paper' && computerSelection=='scissors' ) || (playerSelection== 'scissors' && computerSelection=='rock' )){
         console.log("you lose");
-        return [0,1,0];
+        result.textContent = 'you lose';
+        res[1]++;
+        score.textContent = `you win ${res[0]} times, you lose ${res[1]} times, draw ${res[2]} times`;
+        // return [0,1,0];
     }
     else if ((playerSelection== 'rock' && computerSelection=='scissors' ) || (playerSelection== 'paper' && computerSelection=='rock' ) || (playerSelection== 'scissors' && computerSelection=='paper' )){
         console.log("you win");
-        return [1,0,0];
+        result.textContent = 'you win';
+        res[0]++;
+        
+        score.textContent = `you win ${res[0]} times, you lose ${res[1]} times, draw ${res[2]} times`;
+        // return [1,0,0];
     }
     else if (playerSelection==computerSelection) {
         console.log("draw");
-        return [0,0,1];
+        result.textContent = 'draw';
+        res[2]++;
+        score.textContent = `you win ${res[0]} times, you lose ${res[1]} times, draw ${res[2]} times`;
+        // return [0,0,1];
     }
+    
 }
 
-function game(){
-    res = [0,0,0];
-    for (let i=0; i<5; i++){
-        playRound();
-        for (let j=0; j<3; j++){
-            res[j]+=playRound()[j];
-        }
-    }
-    if (res[0]>res[1]){
-        console.log("End:you win");
-    }
-    else if (res[0]<res[1]){
-        console.log("End:you lose");
-    }
-    else{
-        console.log("End:draw");
-    }
-   console.log(`you win ${res[0]} times, you lose ${res[1]} times, draw ${res[2]} times`);
-   return;
-}
-game();
